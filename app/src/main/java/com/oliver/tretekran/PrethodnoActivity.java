e0a77268a72d88aa42c23cd3c447d3ef3f89c417
markodonovski <markodonovski@live.com>
2018-04-29 17:07:59 +0200

    proba

--- app/src/main/java/com/oliver/tretekran/PrethodnoActivity.java
+++ app/src/main/java/com/oliver/tretekran/PrethodnoActivity.java
@@ -3,11 +3,14 @@ package com.oliver.tretekran;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class PrethodnoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prethodno);
+        ButterKnife.bind(this);
     }
 }
